8cbff9af335d72fd14ea6bd7bed30ea5aea48d60
Ibrahim.9 <ibrahimbrahim9@gmail.com>
2023-12-05 16:06:04 +0100

    upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+// importing the main styles for the application
 import "./App.css";
-// import React from "react";
+
+// importing individual components used in the App
 import Name from "./components/Name";
 import Price from "./components/Price";
 import Description from "./components/Description";
 import Image from "./components/Image";
+
+// importing Bootstrap's CSS and Card component
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card } from "react-bootstrap";
 
+// The main component for the entire application
 function App() {
   const userName = "Stranger";
 
+  // Rendering the main structure of the application
   return (
     <div className="App">
+      {/* Background video for visual appeal */}
       <video
         id="background-video"
         src="/backgroundVideo.mp4"
@@ -20,6 +27,7 @@ function App() {
         muted
       />
 
+      {/* Bootstrap Card component for displaying product information */}
       <Card
         style={{ width: "18rem" }}
         className="mx-auto my-2 p-2 bg-dark bg-gradient text-light"
@@ -35,6 +43,7 @@ function App() {
           </Card.Text>
         </Card.Body>
       </Card>
+      {/* Greeting message with the default or provided username */}
       <h3 className="text-light">Hello there {userName}!</h3>
     </div>
   );
